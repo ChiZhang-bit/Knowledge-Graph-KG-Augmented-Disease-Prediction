8d07ab4d2decd59eeef02b9cb7924009149ff00b
ChiZhang <1061823234@qq.com>
2023-12-15 04:34:07 +0800

    GAT

--- src/train.py
+++ src/train.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import pickle
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 from torch.utils.data import DataLoader
 from Dataset import DiseasePredDataset, read_data
@@ -60,7 +61,7 @@ def evaluate(eval_model, dataloader, device):
     return macro_auc, micro_auc, precision_mean, recall_mean, f1_mean
 
 
-def kg_loss(output, target, model, A, beta, batch_size):
+def kg_loss(output, target, model, adj, beta, batch_size):
     """
     增添的知识图谱关系的loss
     A: 关系邻接矩阵
@@ -76,12 +77,12 @@ def kg_loss(output, target, model, A, beta, batch_size):
     W_iz = W[hidden_size:2 * hidden_size, :]
     W_in = W[2 * hidden_size:3 * hidden_size, :]
     W_matrix = [W_ir, W_iz, W_in]
-
+    
     relationship_loss = 0
     # A: (input_size , input_size) 01matrix
     for i in range(W_ir.shape[0]):
         for j in range(i, W_ir.shape[0]):
-            if A[i, j] == 1:
+            if adj[i, j] == 1:
                 for weight in W_matrix:
                     relationship_loss += torch.norm(weight[i] - weight[j], 2) ** 2
     total_loss = loss1 + (beta / batch_size) * relationship_loss  # 这里要除以batch_size
@@ -97,8 +98,7 @@ def main():
     split_train_point = int(len(features) * 6.7 / 10)
     split_test_point = int(len(features) * 8.7 / 10)
     train_features, train_labels = features[:split_train_point], labels[:split_train_point]
-    test_features, test_labels = features[split_train_point:split_test_point], labels[
-                                                                               split_train_point:split_test_point]
+    test_features, test_labels = features[split_train_point:split_test_point], labels[split_train_point:split_test_point]
     valid_features, valid_labels = features[split_test_point:], labels[split_test_point:]
 
     train_data = DiseasePredDataset(train_features, train_labels)
@@ -139,7 +139,9 @@ def main():
     loss_fn = nn.CrossEntropyLoss()
     optimzer = optim.Adam(model.parameters(), lr=args.lr)
     model = model.to(device)
-
+    adj = torch.zeros(size = (2850, 2850))
+    with open('adjacent_matrix_01.pkl', 'rb') as f:
+        adj = pickle.load(f)
     best_eval_macro_auc = 0
     best_eval_epoch = 0
 
@@ -154,12 +156,13 @@ def main():
             x, y = x.to(device), y.to(device)
             optimzer.zero_grad()
             output = model(x)
-            loss = loss_fn(output, y)
             # 这里还需要补充邻接矩阵的信息，之前只使用CrossEntropyLoss
-            # loss = kg_loss(output, y, model, A=[1], beta=args.beta, batch_size=args.batch_size)
+            loss = kg_loss(output, y, model, adj, beta=args.beta, batch_size=args.batch_size)
+            # loss = loss_fn(output, y)
             loss.backward()
             optimzer.step()
-            # llprint('\rtraining step: {} / {}'.format(idx, len(train_loader)))
+            llprint('\rtraining step: {} / {}'.format(idx, len(train_loader)))
+            input()
             total_loss += loss.item()
 
         avg_loss = total_loss / len(train_loader)
